cc846fc65a872897dd51c82ff883401b286dc3ea
Kallupurakal Joshua <52397105+jokallu431@users.noreply.github.com>
2023-06-13 17:03:17 +0530

    delete files

--- new_dts/user/dbcon.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn=mysqli_connect("localhost","root","","dts");
-if(!$conn){
-	die("Database connection error");
-	
-}
-?>
--- new_dts/user/signout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    // unset($_SESSION['staff_id']);
-    // unset($_SESSION['password']);
-    session_unset();
-    header("location:../index.php");
-    
-?>
